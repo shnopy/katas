015e07f25a84635c969f773353e567b0baa376bb
shnopy <shnopy319@gmail.com>
2020-08-18 20:18:11 +0100

    :tada: Added Kata Solutions!

--- fix-string-case/index.js
+++ fix-string-case/index.js
@@ -1,5 +1,7 @@
 function solve(s) {
-  return s
+  let count_high = 0, count_low = 0;
+  for (char in s) s[char] === s[char].toUpperCase() ? count_high++ : count_low++;
+  return count_high > count_low ? s.toUpperCase() : s.toLowerCase();
 }
 
 console.log(solve("code"))
